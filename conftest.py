1720d68c7f977ecdd22811cfdafbcb5e7a99bda1
taleksovska <taleksovska@ibasis.net>
2025-07-28 12:24:34 +0200

    Add a check in conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+import sys
 
 pytest_plugins = [
     u'ckan.tests.pytest_ckan.ckan_setup',
     u'ckan.tests.pytest_ckan.fixtures',
 ]
+
+if 'ckan' in sys.modules:
+    ckan = sys.modules['ckan']
+    if not hasattr(ckan, '__version__'):
+        ckan.__version__ = '2.11.0'
