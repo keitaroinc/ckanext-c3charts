3aff4f82c9adafeef80ebe9447f62ae803188ba5
taleksovska <taleksovska@ibasis.net>
2025-07-28 14:53:31 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
+
 import sys
+import types
+
+# Patch ckan.__version__ before any other CKAN imports
+def patch_ckan_version():
+    if 'ckan' in sys.modules:
+        ckan = sys.modules['ckan']
+    else:
+        ckan = types.ModuleType('ckan')
+        sys.modules['ckan'] = ckan
+
+    if not hasattr(ckan, '__version__'):
+        ckan.__version__ = '2.11.0'
+
+patch_ckan_version()
 
 pytest_plugins = [
     u'ckan.tests.pytest_ckan.ckan_setup',
     u'ckan.tests.pytest_ckan.fixtures',
 ]
-
-def try_patch_ckan_version():
-    try:
-        import ckan
-        if not hasattr(ckan, '__version__'):
-            ckan.__version__ = '2.11.0'
-    except ImportError:
-        pass
-
-def pytest_load_initial_conftests(args):
-    try_patch_ckan_version()
