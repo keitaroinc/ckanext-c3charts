636d9091b45470cf10df336cf76a4878973ae2ff
taleksovska <taleksovska@ibasis.net>
2025-07-28 15:41:05 +0200

    Use importlib to avoid double loading

--- conftest.py
+++ conftest.py
@@ -1,22 +1,13 @@
-# -*- coding: utf-8 -*-
-
 import sys
-import types
+import importlib
 
-# Patch ckan.__version__ before any other CKAN imports
-def patch_ckan_version():
-    if 'ckan' in sys.modules:
-        ckan = sys.modules['ckan']
-    else:
-        ckan = types.ModuleType('ckan')
-        sys.modules['ckan'] = ckan
-
-    if not hasattr(ckan, '__version__'):
-        ckan.__version__ = '2.11.0'
-
-patch_ckan_version()
+def safe_register_plugin(name):
+    module = sys.modules.get(name)
+    if not module:
+        module = importlib.import_module(name)
+    return module
 
 pytest_plugins = [
-    u'ckan.tests.pytest_ckan.ckan_setup',
-    u'ckan.tests.pytest_ckan.fixtures',
+    safe_register_plugin('ckan.tests.pytest_ckan.ckan_setup'),
+    safe_register_plugin('ckan.tests.pytest_ckan.fixtures'),
 ]
