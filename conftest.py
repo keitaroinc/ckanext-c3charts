ba1034e1b0ccd3f330fe86eb5ec20d890a7856db
taleksovska <taleksovska@ibasis.net>
2025-07-29 17:04:45 +0200

    Update test config

--- conftest.py
+++ conftest.py
@@ -1,9 +1,4 @@
-import pytest
-import ckan
-
-@pytest.fixture(autouse=True)
-def mock_ckan_version():
-    ckan.__version__ = '2.10'
+# -*- coding: utf-8 -*-
 
 pytest_plugins = [
     u'ckan.tests.pytest_ckan.ckan_setup',
